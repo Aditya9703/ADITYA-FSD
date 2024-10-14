09a2b0abaf0508227e7b2666022d80059ff5df74
Aditya9703 <kushwahaditya054@gmail.com>
2024-10-14 13:06:53 +0530

    day6

---
+++ Day6/script.js
@@ -0,0 +1,29 @@
+// let a=()=>{
+//     console.log("Arrow Function");
+// };
+//a();
+// 
+// let c=(x,y)=>
+// {
+//     return x+y;
+// }
+// let res=c('Rahul',"Ravi");
+// console.log(res)
+function display()
+{
+    // let p1=document.querySelector('p');
+    // let p1=document.querySelectorAll('p');
+    // p1.innerHTML="I am in para";
+    // p1.style.backgroundColor="red";
+    // p1[0].innerHTML="I am in para1";
+    // p1[0].style.backgroundColor="red"
+    // p1[1].innerHTML="I am in para2";
+    // p1[1].style.backgroundColor="green"
+    // const header=document.querySelector('h1');
+    // header.textContent="now i am change";
+    //set attribute is usev to change the link
+// const newDiv=document.createElement('div');
+// newDiv.textContent='This is a new div';
+// const body=document.querySelector('body');
+// body.appendChild(newDiv);
+};
