b3305fa51dfd8763ff7b39cc730cdecc432673a0
Aditya9703 <kushwahaditya054@gmail.com>
2024-10-16 13:03:12 +0530

    day7

---
+++ Day7/script.js
@@ -0,0 +1,46 @@
+let num=[1,2,3,4,45,5,45];
+// let newnum=num.map(x=>x*2);
+// console.log(newnum)
+
+// let evennum=num.filter(x=>x%2==0);
+// console.log(evennum);
+// let sum=num.reduce((x,y)=>x+y,0);
+// console.log(sum);
+// let pro=num.reduce((x,y)=>x*y,1);
+// console.log(pro);
+const students=[
+    { name:'Alice',score: 50},
+    { name:'Bob',score:65},
+    { name:'Charlie',score:80},
+    { name:'David',score:45},
+    { name: 'Rahul',score:50},
+];
+// let scores=students.map(x=>x.score);
+// console.log(scores);
+// let sum=scores.reduce((x,y)=>x+y,0) x is accumulator and y is score
+// console.log(sum);
+// console.log(students.map(x=>x.score).reduce((x,y)=>x+y,0))
+// console.log(students.filter(x=>x.score>60).map((x)=>x.score+10).reduce((x,y)=>x+y,0));
+// let nums=[12,3,3,3,4,4,4,4,98];
+// nums.forEach((x)=>console.log(x*3));
+
+// sayhello=()=>
+// {
+// console.log("I am in hello functions");
+// };
+// console.log("start");
+// setTimeout(sayhello,3000);
+// console.log("End");
+// setTimeout(sayhello,6000);
+console.log("start");
+setTimeout(()=>{
+    console.log("first task completed");
+    setTimeout(()=>
+    {
+        console.log("second task completed");
+        setTimeout(()=>
+        {
+            console.log("third")
+        })
+    })
+}
