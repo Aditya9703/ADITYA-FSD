ee6c52108a3a8befc9e8090cc37d956a36457405
Aditya9703 <kushwahaditya054@gmail.com>
2024-11-18 12:56:22 +0530

    movie

---
+++ Day12/netflix-clone/src/movies.js
@@ -0,0 +1,38 @@
+const movies=[
+    {
+            id:1,
+            image :"https://rukminim2.flixcart.com/image/612/612/xif0q/movie/4/f/r/2009-dvd-eagle-hindi-tarzan-the-wonder-car-original-imahfdpjpqg85j5d.jpeg?q=70" ,
+            title:"TARZAN-THE WONDER CAR",
+            language:"Hindi",
+            cost:250,
+    },
+        {
+                id:2,
+                image :"https://rukminim2.flixcart.com/image/612/612/xif0q/movie/4/f/r/2009-dvd-eagle-hindi-tarzan-the-wonder-car-original-imahfdpjpqg85j5d.jpeg?q=70" ,
+                title:"TARZAN-THE WONDER CAR",
+                language:"Hindi",
+                cost:200,
+        },
+        {
+            id:3,
+            image :"https://rukminim2.flixcart.com/image/612/612/xif0q/movie/4/f/r/2009-dvd-eagle-hindi-tarzan-the-wonder-car-original-imahfdpjpqg85j5d.jpeg?q=70" ,
+            title:"TARZAN-THE WONDER CAR",
+            language:"Hindi",
+            cost:350,
+    },
+    {
+        id:4,
+        image :"https://rukminim2.flixcart.com/image/612/612/xif0q/movie/4/f/r/2009-dvd-eagle-hindi-tarzan-the-wonder-car-original-imahfdpjpqg85j5d.jpeg?q=70" ,
+        title:"TARZAN-THE WONDER CAR",
+        language:"Hindi",
+        cost:250,
+},
+{
+    id:5,
+    image :"https://rukminim2.flixcart.com/image/612/612/xif0q/movie/4/f/r/2009-dvd-eagle-hindi-tarzan-the-wonder-car-original-imahfdpjpqg85j5d.jpeg?q=70" ,
+    title:"TARZAN-THE WONDER CAR",
+    language:"Hindi",
+    cost:300,
+},
+];
+export default movies;
