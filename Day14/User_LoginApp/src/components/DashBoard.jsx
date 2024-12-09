abc73c016827ad273296e80c623429a7e90ed3d4
Aditya9703 <kushwahaditya054@gmail.com>
2024-12-09 13:06:38 +0530

    14

--- Day14/User_LoginApp/src/components/DashBoard.jsx
+++ Day14/User_LoginApp/src/components/DashBoard.jsx
@@ -1,10 +1,6 @@
+import React from "react";
 
-
-functio DashBoard() {
-    return (
-        <>
-        </>
-    )
-}
-
-export default DashBoard;
+const Dashboard = () => {
+  return <div>Dashboard</div>;
+};
+export default Dashboard;
